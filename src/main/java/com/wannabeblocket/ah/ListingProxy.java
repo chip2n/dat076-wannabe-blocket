888a1d4855e8b508d3439fd2f60067fa8f75072d
Andreas Arvidsson <andreas.arvidson@gmail.com>
2013-10-15 19:58:12 +0200

    Added some additional fields for the Listing JSON object.

--- src/main/java/com/wannabeblocket/ah/ListingProxy.java
+++ src/main/java/com/wannabeblocket/ah/ListingProxy.java
@@ -1,13 +1,16 @@
 package com.wannabeblocket.ah;
 
 import com.wannabeblocket.model.Listing;
+import java.text.SimpleDateFormat;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- *
+ * Class for turning a Listing object magically into a XML/JSON string
+ * of pure love and understanding.
+ * 
  * @author Andreas Arvidsson
  */
 @XmlRootElement(name = "listing")
@@ -21,8 +24,24 @@ public class ListingProxy {
         this._listing = listing;
     }
     
+    @XmlElement(name = "id")
+    public Long getId() {
+        return _listing.getId();
+    }
+    
     @XmlElement(name = "seller")
     public String getSeller() {
         return _listing.getSeller().getUserName();
+    }
+    
+    @XmlElement(name = "description")
+    public String getDescription() {
+        return _listing.getDescription();
+    }
+    
+    @XmlElement(name = "end-time")
+    public String getEndTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd:HH:mm:ss");
+        return dateFormat.format(_listing.getEndingTime());
     }
 }
