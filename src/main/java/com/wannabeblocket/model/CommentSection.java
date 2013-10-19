264416cc66ab888da78288275ae9fdbc36d4932d
Joppe Widstam <joppewidstam@hotmail.com>
2013-10-19 15:38:40 +0200

    Fixed databasequeries.

--- src/main/java/com/wannabeblocket/model/CommentSection.java
+++ src/main/java/com/wannabeblocket/model/CommentSection.java
@@ -11,6 +11,6 @@ public class CommentSection extends AbstractDAO<Comment, Long> {
     
     public List<Listing> getCommentsByListing(Listing listing) {
        EntityManager em = emf.createEntityManager();
-       return em.createQuery("SELECT c FROM COMMENT WHERE listing = " + listing.getId()).getResultList();
+       return em.createQuery("SELECT c FROM Comment c WHERE c.category = :id").setParameter("id", listing).getResultList();
     }
 }
