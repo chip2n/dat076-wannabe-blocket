9686193cadc60a8a893f5df664e7b16889ca5e0a
Sebastian Lagermann <barafinkel@gmail.com>
2013-10-19 16:30:08 +0200

    Adding search query for search.

--- src/main/webapp/resources/js/auctionhouse.js
+++ src/main/webapp/resources/js/auctionhouse.js
@@ -5,7 +5,7 @@ var AuctionHouse = function(url) {
 AuctionHouse.prototype = (function() {
     return {
         getRange: function(first, nItems) {
-            return $.getJSON("http://localhost:8080/ah/rs/auctions/range?fst=" + first + "&max=" + nItems);
+            return $.getJSON("http://localhost:8080/ah/rs/auctions/range?fst=" + first + "&max=" + nItems + "&query=glad");
         },
         getCount: function() {
             return $.getJSON("http://localhost:8080/ah/rs/auctions/count");
