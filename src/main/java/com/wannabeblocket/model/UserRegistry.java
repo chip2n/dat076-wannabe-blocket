66317e4438a09332dfb49115d3cc1869203e865c
Christoffer Andersson <chrander@student.chalmers.se>
2013-10-20 11:59:52 +0200

    Changed login method in UserRegistry.java Added email Column to Account.java Updated tests for accounts

--- src/main/java/com/wannabeblocket/model/UserRegistry.java
+++ src/main/java/com/wannabeblocket/model/UserRegistry.java
@@ -1,20 +1,13 @@
 package com.wannabeblocket.model;
 
-import java.util.List;
-import javax.persistence.EntityManager;
-
 public class UserRegistry extends AbstractDAO<Account, String> {
 
     public UserRegistry(String puName) {
         super(Account.class, puName);
     }
 
-    public boolean login(String userName, String password) {
-        if (find(userName).getPassword().equals(password)) {
-            return true;
-        } else {
-            return false;
-        }
-
+    public Account login(String userName, String password) {
+        Account tmp = find(userName);
+        return tmp != null && tmp.getPassword().equals(password) ? tmp : null;
     }
 }
--- src/test/java/com/wannabeblocket/test/ModelTests.java
+++ src/test/java/com/wannabeblocket/test/ModelTests.java
@@ -7,9 +7,12 @@ package com.wannabeblocket.test;
 import com.wannabeblocket.model.Account;
 import com.wannabeblocket.model.AuctionHouse;
 import com.wannabeblocket.model.BiddingHistory;
+import com.wannabeblocket.model.Category;
 import com.wannabeblocket.model.CommentSection;
+import com.wannabeblocket.model.Listing;
 import com.wannabeblocket.model.Shop;
 import com.wannabeblocket.model.UserRegistry;
+import java.util.Date;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -28,11 +31,33 @@ public class ModelTests {
     private static CommentSection csec;
     private static BiddingHistory bhist;
     
+    
+    static final Account[] accs = {
+            new Account("Olle", "12345"),
+            new Account("Bengt", "54321"),
+            new Account("Lisa", "hemligt"),
+            new Account("Greger", "ange lösenord")
+    };
+            
     public ModelTests() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        shop = Shop.getInstance(Shop.Mode.Debug);
+        usrreg = shop.getUserRegistry();
+        ah = shop.getAuctionHouse();
+        csec = shop.getCommentSection();
+        bhist = shop.getBiddingHistory();
+        
+        // add users
+        assert(usrreg.getCount() == 0);
+        
+        for(int i = 0; i < accs.length; ++i)
+        {
+            usrreg.add(accs[i]);
+            assert(usrreg.getCount() == i+1);
+        }
     }
     
     @AfterClass
@@ -41,11 +66,6 @@ public class ModelTests {
     
     @Before
     public void setUp() {
-        shop = Shop.getInstance(Shop.Mode.Debug);
-        usrreg = shop.getUserRegistry();
-        ah = shop.getAuctionHouse();
-        csec = shop.getCommentSection();
-        bhist = shop.getBiddingHistory();
     }
     
     @After
@@ -55,20 +75,13 @@ public class ModelTests {
     @Test
     public void accountTests()
     {   
-        // add users
-        Account[] accs = {
-            new Account("Olle", "12345"),
-            new Account("Bengt", "54321"),
-            new Account("Lisa", "hemligt"),
-            new Account("Greger", "ange lösenord")
-        };
+        // test login method
+        assert(usrreg.login(accs[0].getUserName(), accs[0].getPassword())
+                .getUserName().equals(accs[0].getUserName()));
         
-        for(Account a : accs)
-            usrreg.add(a);      
-        
-        // check that all accounts got added
-        assert(usrreg.getCount() == accs.length);
-        
+        assert(null == usrreg.login(
+                accs[0].getUserName(), "wrong password"));
+       
         // test so no users can have the same username
         int dindex = 2; // index to user
         
@@ -82,8 +95,17 @@ public class ModelTests {
     
     @Test
     public void test()
-    {
+    {   
+        Category[] cats = {
+            new Category("Gammal skit"),
+            new Category("Stulet"),
+            new Category("Barn"),
+            new Category("Övrigt")
+        };
         
+        Listing[] lList = {
+            new Listing(accs[1], "Trasig stol", "Kommer i bitar.", new Date(), cats[0])
+        };
     }
     
 }
